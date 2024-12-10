acd9bd4186872ccf6ff7834fa0d35d52792141a9
Pedro Marques <83501969+PedroMarquesr@users.noreply.github.com>
2024-12-10 14:15:35 -0300

    Update projetoEstoque.js

--- projetoEstoque.js
+++ projetoEstoque.js
@@ -1,4 +1,4 @@
-import entradaDados from "readline-sync";
+import entradaDados from "readline-sync"
 let produtos = [
   {
     id: 1,
@@ -96,13 +96,10 @@ let produtos = [
     quantidade: 15,
     preco: 69.9,
   },
-];
+]
 let novoId = produtos.reduce((maxId, produto) => {
-  return Math.max(maxId, produto.id);
-}, 0);
-let valorTotalativo = produtos.reduce((total, produto) => {
-  return total + produto.quantidade * produto.preco;
-}, 0);
+  return Math.max(maxId, produto.id + 1)
+}, 0)
 
 // Declarei as funcionalidades para não precisar reescreve-las sempre
 let funcionalidades = `1) Mostrar o estoque completo. 
@@ -111,20 +108,20 @@ let funcionalidades = `1) Mostrar o estoque completo.
 4) Atualizar a quantidade de um produto existente.
 5) Calcular o valor total do estoque.
 6) Remover produto do estoque.
-7) Encerrar o programa`;
+7) Encerrar o programa`
 
-console.log("\n\n");
-console.log("Velas aromáticas - Velas de Barros");
-console.log("\n\n");
-console.log(`Funcionalidades ↓↓`.padStart(26, ".").padEnd(34, "."));
-console.log("\n");
+console.log("\n\n")
+console.log("Velas aromáticas - Velas de Barros")
+console.log("\n\n")
+console.log(`Funcionalidades ↓↓`.padStart(26, ".").padEnd(34, "."))
+console.log("\n")
 
-let escolha; // Declaro a variavel escolha, para utilizar no "do"
+let escolha // Declaro a variavel escolha, para utilizar no "do"
 
 do {
   // O programa fará ↓↓↓
-  console.log(funcionalidades);
-  console.log("\n");
+  console.log(funcionalidades)
+  console.log("\n")
   escolha = Number(
     entradaDados
       .question(
@@ -132,87 +129,132 @@ do {
 `
       )
       .trim()
-  );
+  )
 
   if (escolha >= 1 && escolha <= 7) {
     switch (escolha) {
       case 1:
-        console.log("Relatório de estoque completo");
+        console.log("Relatório de estoque completo")
         for (let i = 0; i < produtos.length; i++) {
-          let id_produto = produtos[i].id;
-          let nome_produto = produtos[i].nome;
-          let quantidade_produto = produtos[i].quantidade;
-          console.log(`Id: ${id_produto}`);
-          console.log(`Nome: ${nome_produto}`);
-          console.log(
-            `Quantidade disponivel em estoque: ${quantidade_produto}`
-          );
-          console.log("\n\n");
+          let id_produto = produtos[i].id
+          let nome_produto = produtos[i].nome
+          let quantidade_produto = produtos[i].quantidade
+          console.log(`Id: ${id_produto}`)
+          console.log(`Nome: ${nome_produto}`)
+          console.log(`Quantidade disponivel em estoque: ${quantidade_produto}`)
+          console.log("\n\n")
         }
-        console.log("Deseja realizar outra tarefa?");
-        console.log("\n");
-        break;
+        console.log("Deseja realizar outra tarefa?")
+        console.log("\n")
+        break
       case 2:
-        let verificar_outro_item; // Controla o loop de verificação de itens
+        let verificar_outro_item // Controla o loop de verificação de itens
 
         do {
           let id_informado = Number(
             entradaDados.question("Digite o Id do produto:\n").trim()
-          );
-          let indice_produto = id_informado - 1; // Conversão de id em indice
-          console.log("\n");
+          )
+          let indice_produto = id_informado - 1 // Conversão de id em indice
+          console.log("\n")
 
           if (indice_produto >= 0 && indice_produto < produtos.length) {
-            console.log(`Id: ${produtos[indice_produto].id}`);
-            console.log(`Produto: ${produtos[indice_produto].nome}`);
+            console.log(`Id: ${produtos[indice_produto].id}`)
+            console.log(`Produto: ${produtos[indice_produto].nome}`)
             console.log(
               `Quantidade em estoque: ${produtos[indice_produto].quantidade}`
-            );
-            console.log("\n\n");
+            )
+            console.log("\n\n")
           } else {
-            console.log("Produto não encontrado. Verifique o ID informado.");
-            console.log("\n");
+            console.log("Produto não encontrado. Verifique o ID informado.")
+            console.log("\n")
           }
 
           // Pergunta se o usuário deseja verificar outro item
           verificar_outro_item = Number(
             entradaDados
-              .question(
-                `Deseja verificar outro item?
-                1) Sim
-                2) Não\n`
-              )
+              .question("Deseja verificar outro item ? \n1) Sim\n2) Não\n")
               .trim()
-          );
-          console.log("\n\n");
-        } while (verificar_outro_item !== 2);
+          )
+          console.log("\n\n")
+        } while (verificar_outro_item !== 2)
 
-        console.log("Deseja realizar outra tarefa?");
-        console.log("\n");
-        break;
+        console.log("Deseja realizar outra tarefa?")
+        console.log("\n")
+        break
       case 3:
-        break;
+        console.log("\n")
+        console.log("Cadastro de item".padEnd(34, "."))
+        console.log("\n")
+
+        let adicionar_outro_item
+
+        do {
+          let nome_nproduto = entradaDados.question(
+            `Informe o nome do item: \n`
+          )
+          console.log("\n")
+          let preco_nproduto = entradaDados.question(
+            "Informe o preço do item: \n"
+          )
+          console.log("\n")
+          let quantidade_nproduto = entradaDados.question(
+            "Informe a quantidade de entrada no estoque \n"
+          )
+          console.log("\n")
+          let novo_produto = {
+            id: novoId,
+            nome: nome_nproduto,
+            preco: preco_nproduto,
+            quantidade: quantidade_nproduto,
+          }
+          novoId++
+
+          console.log("produto adicionado")
+          console.log(novo_produto)
+
+          produtos.push(novo_produto)
+
+          do {
+            adicionar_outro_item = Number(
+              entradaDados
+                .question("Deseja adicionar outro item?\n1)Sim \n2)Não\n")
+                .trim()
+            )
+            console.log("\n")
+            if (adicionar_outro_item !== 1 && adicionar_outro_item !== 2) {
+              console.log(
+                "Opção invalida \nDigite (1) Para 'Sim' ou (2) para 'Não'\n"
+              )
+            }
+          } while (adicionar_outro_item !== 1 && adicionar_outro_item !== 2)
+        } while (adicionar_outro_item !== 2)
+        console.log("Deseja realizar outra tarefa?")
+        console.log("\n")
+        break
       case 4:
-        console.log("Caso 4");
-        break;
+        console.log("Caso 4")
+        break
       case 5:
-        console.log("\n\n");
+        let valorTotalativo = produtos.reduce((total, produto) => {
+          return total + produto.quantidade * produto.preco
+        }, 0)
+        console.log("\n\n")
         console.log(
           `Valor total de Estoque ativo é de R$${valorTotalativo},00 \n`
-        );
-        console.log("Deseja realizar outra tarefa?");
-        console.log("\n");
-        break;
+        )
+        console.log("Deseja realizar outra tarefa?")
+        console.log("\n")
+        break
       case 6:
-        console.log("Caso 6");
-        break;
+        console.log("Caso 6")
+        break
       case 7:
-        console.log("Aplicação encerrada.");
-        break;
+        console.log("Aplicação encerrada.")
+        break
     }
   } else {
-    console.log("\n");
-    console.log("Digite uma opção valida");
-    console.log("\n");
+    console.log("\n")
+    console.log("Digite uma opção valida")
+    console.log("\n")
   } // O programa fará ↑↑
-} while (escolha !== 7); //Enquanto a escolha não for 6 (Encerrar o programa)
+} while (escolha !== 7) //Enquanto a escolha não for 6 (Encerrar o programa)
